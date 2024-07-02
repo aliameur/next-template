56208540f625515fbdecffade0b0cc1cd56fc8bb
Ali Ameur <aliameur169@gmail.com>
2024-07-02 05:15:06 +0100

    chore: center initial home page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       Digitique Next.js Template
     </main>
   );
